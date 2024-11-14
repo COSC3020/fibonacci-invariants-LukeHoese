5e5ef64608edb91e18aa264e7cd52427ade9207c
Luke Hoese <lukehoese@gmail.com>
2024-11-14 12:51:22 -0700

    update1

--- code.js
+++ code.js
@@ -1,3 +1,30 @@
-function fib(n) {
-    return n;
+function fib(n) 
+{
+    var result = [0];
+    if (n === 0)
+    {
+        return result;
+    }
+    result = [0, 1];
+    if (n === 1)
+    {
+        return result;
+    }
+
+    function calcFib(i)
+    {
+        if (i >= n)
+        {
+        return;
+        }
+
+        result[i] = result[i - 1] + result[i - 2];
+
+        calcFib(i + 1);
+    }
+
+    calcFib(2);
+    return result;
 }
+
+console.log(fib(10));
